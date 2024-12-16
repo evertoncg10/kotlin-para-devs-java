77b53cf758a53794feeb525bcfa9438ccad02b6b
evertoncg10 <evertoncg10@outlook.com>
2024-12-16 09:57:47 -0300

    :sparkles: feat: Seção 4 - Aula03 - Tipos de Variáveis em Kotlin.

---
+++ src/main/kotlin/br/com/everton/section04_0403/KotlinTypes.kt
@@ -0,0 +1,25 @@
+package br.com.everton.section04_0403
+
+fun main() {
+    val myPhrase = "I'm a String"
+    val myPi = 3.14
+    val myAnswer = 42
+
+    val aString: String
+    val aDouble: Double
+    val aInt: Int
+    val aBoolean: Boolean
+
+    val myDouble = 1.999
+    val myFloat = 1.9f
+    val myLong = 9977989623232L
+    val myInt = 199
+    val myShort: Short = 12
+    val myByte: Byte = 127
+
+    val longNumber = 123_456_789
+    val theSameNumber = 123456789
+
+    val anInt: Int = 7
+    val aLong: Long = anInt.toLong()
+}
