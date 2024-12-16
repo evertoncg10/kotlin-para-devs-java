030dfac1bf72ef7e173899def20658357e8d275e
evertoncg10 <evertoncg10@outlook.com>
2024-12-16 11:03:57 -0300

    :lipstick: feat: Ajuste conteúdo.

--- src/main/kotlin/br/com/everton/section04/_0406/WhenExpression.kt
+++ src/main/kotlin/br/com/everton/section04/_0406/WhenExpression.kt
@@ -1,24 +1,45 @@
 package br.com.everton.section04._0406
 
 fun main() {
+    /*
+       A expressão when em Kotlin substitui a expressão switch.
+       Ela tem 2 tipos: a com argumento e a sem argumento.
+   */
 
-    var x = 0
+    val pizzasOrdered = 7
 
-    while (x < 10) {
-        println("x = $x")
-        x += 3
+    when (pizzasOrdered) {
+        0 -> println("Not hungry")
+        1 -> println("Hungry")
+        2 -> println("Very hungry")
+        else -> println("Are you sure?")
+    }
+    // Com condições combinadas
+    when (pizzasOrdered) {
+        0 -> println("Not hungry")
+        1,2 -> println("Hungry")
+        3,4,5 -> println("Very hungry")
+        else -> println("Are you sure?")
     }
 
-    do {
-        println("x = $x")
-        x -= 3
-    } while (x > 0)
-
-    for (item in 1..10) {
-        print("$item, ")
+    // Podemos usar condicionais.
+    when (pizzasOrdered) {
+        Math.abs(pizzasOrdered) -> println("Ordered 0 or more pizzas")
+        else -> println("Ordered less than 0")
     }
 
-    for (ch in "Everton") {
-        println(ch)
+    // Utilizando Ranges
+    when (pizzasOrdered) {
+        0 -> println("We need orders")
+        in 1 ..2 -> println("Got some orders")
+        in 5 .. 9 -> println("Business is up")
+        else -> println("Are you sure?")
+    }
+
+    // sem argumentos - Pode ser um substituto para ifs e else
+    when {
+        pizzasOrdered <= 0 -> println("None ordered")
+        pizzasOrdered % 2 == 1 -> println("Odd number ordered")
+        pizzasOrdered % 2 == 0 -> println("Even number ordered")
     }
 }
