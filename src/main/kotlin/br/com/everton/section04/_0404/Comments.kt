b7b06164c0e2f6b2adfe15227c6704289c321001
evertoncg10 <evertoncg10@outlook.com>
2024-12-16 10:04:54 -0300

    :sparkles: feat: Seção 4 - Aula04 - Comentários

---
+++ src/main/kotlin/br/com/everton/section04/_0404/Comments.kt
@@ -0,0 +1,42 @@
+package br.com.everton.section04._0404
+
+fun main() {
+
+    // I am a commentary
+    val myPhrase = "I'm a String"
+    val myPi = 3.14
+    val myAnswer = 42
+
+    /*
+        I am a multiline
+        commentary
+        I am a block comment
+        val aString: String
+        val aDouble: Double
+        val aInt: Int
+        val aBoolean: Boolean
+     */
+
+
+    val myDouble = 1.999
+    val myFloat = 1.9f
+    val myLong = 9977989623232L
+    val myInt = 199
+    val myShort: Short = 12
+    val myByte: Byte = 127
+
+    val longNumber = 123_456_789
+    val theSameNumber = 123456789
+
+    val anInt: Int = 7
+    val aLong: Long = anInt.toLong()
+}
+
+/*
+
+fun fooBar() {
+    /*
+    I am embedded comment
+     */
+}
+*/
