fbb64740b8fb15e599194f53e7624d4cc04da4cb
evertoncg10 <evertoncg10@outlook.com>
2024-12-27 11:01:19 -0300

    :sparkles: feat: Seção 8 - Aula 3 - Lambdas em Kotlin

---
+++ src/main/kotlin/br/com/everton/section08/_0803/Lambdas.kt
@@ -0,0 +1,10 @@
+package br.com.everton.section08._0803
+
+fun main() {
+    val students = getStudents()
+    val combos = students.map { student -> "${student.name} : ${student.age}"}
+    println("Combos: $combos")
+
+    println("The oldest student is: ${students.maxByOrNull { it.age }}")
+    println("Student with longest name is: ${students.filter { it.name.length > 5 }}")
+}
